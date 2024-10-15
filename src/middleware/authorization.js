bd65b31ee10070a7fc73c10ac7ffde0d93d1309a
Rafael Machado <rafmco2@hotmail.com>
2024-10-15 10:53:00 -0400

    style: code format;

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -1,38 +1,40 @@
-module.exports = app => {
+module.exports = (app) => {
   const authorization = async (req, res, next) => {
     // pesquisa usuário
-    const findUser = await app.db("user")
+    const findUser = await app
+      .db("user")
       .where({
         deleted_at: null,
-        login: req.user.login
+        login: req.user.login,
       })
       .first();
 
     // pesquisa Requisição
-    const findRequest = await app.db("request_route")
+    const findRequest = await app
+      .db("request_route")
       .where({
         deleted_at: null,
-        url: req.route.path
+        url: req.route.path,
       })
       .first();
 
     // pesquisar Relacionamento User X Request
     if (findRequest.id && findUser.id) {
-      const findPermission = await app.db("user_request")
+      const findPermission = await app
+        .db("user_request")
         .where({
           deleted_at: null,
           request_id: findRequest ? findRequest.id : undefined,
-          user_id: findUser ? findUser.id : undefined
+          user_id: findUser ? findUser.id : undefined,
         })
         .first();
 
-      if (findPermission && findPermission.id) next()
-      else return res.status(401).send("Usuário Sem Permissão")
+      if (findPermission && findPermission.id) next();
+      else return res.status(401).send("Usuário Sem Permissão");
     } else {
-
-      return res.status(401).send("Usuário Sem Permissão")
+      return res.status(401).send("Usuário Sem Permissão");
     }
   };
 
-  return { authorization: () => authorization }
-}
+  return { authorization: () => authorization };
+};
