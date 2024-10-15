1cc94dc9085d0288ddc7c41f3a7f20c1e4fcc9e1
Rafael Machado <rafmco2@hotmail.com>
2024-10-15 10:50:39 -0400

    style: migrations code format;

--- src/config/database/migrations/20201125090500_user_route.js
+++ src/config/database/migrations/20201125090500_user_route.js
@@ -1,28 +1,30 @@
-const dayjs = require("dayjs")
+const dayjs = require("dayjs");
 
-exports.up = function (knex, Promise) {
-  return knex.schema.createTable("user_route", table => {
-    table.increments("id").primary();
-    table.integer("user_id").unsigned().notNullable();
-    table.integer("route_id").unsigned().notNullable();
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("user_route", table => {
+      table.increments("id").primary();
+      table.integer("user_id").unsigned().notNullable();
+      table.integer("route_id").unsigned().notNullable();
 
-    table.foreign("user_id").references("id").inTable("user");
-    table.foreign("route_id").references("id").inTable("route");
+      table.foreign("user_id").references("id").inTable("user");
+      table.foreign("route_id").references("id").inTable("route");
 
-    table.string("created_by", 15);
-    table.timestamp("created_at").nullable();
-    table.string("deleted_by", 15);
-    table.timestamp("deleted_at").nullable();
-  }).then(() => {
-    return knex("user_route").insert({
-      created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
-      created_by: "MIGRATE SYSTEM",
-      route_id: 1,
-      user_id: 1
+      table.string("created_by", 15);
+      table.timestamp("created_at").nullable();
+      table.string("deleted_by", 15);
+      table.timestamp("deleted_at").nullable();
     })
-  });
+    .then(() => {
+      return knex("user_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        route_id: 1,
+        user_id: 1,
+      });
+    });
 };
 
-exports.down = function (knex, Promise) {
-  return knex.schema.dropTable("user_route")
+exports.down = function (knex) {
+  return knex.schema.dropTable("user_route");
 };
--- src/config/database/migrations/20201125090600_request_route.js
+++ src/config/database/migrations/20201125090600_request_route.js
@@ -1,66 +1,235 @@
-const dayjs = require("dayjs")
+const dayjs = require("dayjs");
 
-exports.up = function (knex, Promise) {
-  return knex.schema.createTable("request_route", table => {
-    table.increments("id").primary();
-    table.integer("route_id").unsigned().notNullable();
-    table.string("description", 250).nullable();
-    table.string("url", 250).nullable();
-    table.string("note", 250).nullable();
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("request_route", table => {
+      table.increments("id").primary();
+      table.integer("route_id").unsigned().notNullable();
+      table.string("description", 250).nullable();
+      table.string("url", 250).nullable();
+      table.string("note", 250).nullable();
 
-    table.foreign("route_id").references("id").inTable("route");
+      table.foreign("route_id").references("id").inTable("route");
 
-    table.string("created_by", 15);
-    table.timestamp("created_at").nullable();
-    table.string("updated_by", 15);
-    table.timestamp("updated_at").nullable();
-    table.string("deleted_by", 15);
-    table.timestamp("deleted_at").nullable();
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user/listar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user/editar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user/deletar/:id", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user/exibir/:id", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user-route/telas-usuario/:userId", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user-route/usuarios-tela/:routeId", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user-route/salvar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user-route/deletar/:id", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user-request/listar/:userId", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user-request/salvar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/user-request/deletar/:id", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/route/listar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/route/salvar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/route/editar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/route/deletar/:id", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/route/exibir/:id", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/request-route/listar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/request-route/salvar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/request-route/editar", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/request-route/deletar/:id", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  }).then(() => {
-    return knex("request_route").insert({ route_id: 1, url: "/request-route/exibir/:id", description: "", note: "", created_by: "MIGRATE SYSTEM", created_at: dayjs().format("YYYY-MM-DD HH:mm:ss") });
-  });
+      table.string("created_by", 15);
+      table.timestamp("created_at").nullable();
+      table.string("updated_by", 15);
+      table.timestamp("updated_at").nullable();
+      table.string("deleted_by", 15);
+      table.timestamp("deleted_at").nullable();
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user/listar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user/editar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user/deletar/:id",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user/exibir/:id",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user-route/telas-usuario/:userId",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user-route/usuarios-tela/:routeId",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user-route/salvar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user-route/deletar/:id",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user-request/listar/:userId",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user-request/salvar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/user-request/deletar/:id",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/route/listar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/route/salvar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/route/editar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/route/deletar/:id",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/route/exibir/:id",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/request-route/listar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/request-route/salvar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/request-route/editar",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/request-route/deletar/:id",
+      });
+    })
+    .then(() => {
+      return knex("request_route").insert({
+        created_at: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+        created_by: "MIGRATE SYSTEM",
+        description: "",
+        note: "",
+        route_id: 1,
+        url: "/request-route/exibir/:id",
+      });
+    });
 };
 
-exports.down = function (knex, Promise) {
-  return knex.schema.dropTable("request_route")
+exports.down = function (knex) {
+  return knex.schema.dropTable("request_route");
 };
